eff4406c41b8d292f34cbf372e01651c86583f4c
Akshitha <akshithanagaraj16@gmail.com>
2026-08-28 18:53:23 +0530

    Strings concept 28-08-26

---
+++ strings/String_memory.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class String_memory {
+
+	public static void main(String[] args) {
+		
+			    //scp
+				String str1 = "Hello";//100
+				String str2 = "Hello";//100
+				//heap
+				String str3 = new String("Hello");//200
+				String str4 = new String("Hello");//300
+				System.out.println(str1==str2);
+				System.out.println(str3==str4);
+	}
+
+}
